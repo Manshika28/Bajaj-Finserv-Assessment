245caeefdf70aa8cf40454f2f87ecbee4f19028f
Manshika28 <157523545+Manshika28@users.noreply.github.com>
2026-02-10 12:54:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('API LIVE - ROUTES LOADED');
+});
 
 const OFFICIAL_EMAIL = "manshika0732.be23@chitkara.edu.in";
 
@@ -122,3 +125,4 @@ app.post('/bfhl', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
